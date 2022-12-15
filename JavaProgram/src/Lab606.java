83fc023ff3183fe1a13383f6f1fda190a10c0494
Chonticha178 <Chontichakop@gmail.com>
2022-12-15 16:25:21 +0700

    Week6[15-12-2022]

---
+++ JavaProgram/src/Lab606.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+public class Lab606 {
+
+	public static void main(String[] args) {
+		int nums[] = {25, 78, 41, 22, 36, 85,37};
+		int index = Integer.parseInt(JOptionPane.showInputDialog("Input index of array:"));
+		while(checkIndex(nums,index)) {
+			index = Integer.parseInt(JOptionPane.showInputDialog("Input index of array,again:"));
+		}
+		String prevData = (prevData(nums,index)!=-1?"Paevious data,nums["+(index-1)+"] is "+prevData(nums,index):"NO previos data");
+		String nextData = (nextData(nums,index)!=-1)?"Next data,nums["+(index+1)+"] is "+nextData(nums,index):"NO next data";
+		JOptionPane.showMessageDialog(null, "Current data, nums["+index+"] is "+currentData(nums,index)+"\n"+prevData+"\n"+nextData);
+		
+	}
+	public static boolean checkIndex(int[] nums,int index) {
+		return index <0 || index>nums.length-1;
+	}
+	public static int currentData(int[] nums,int index) {
+		return nums[index];
+	}
+	public static int prevData(int[] nums,int index) {
+		return index == 0?-1:nums[index-1];
+	}
+	public static int nextData(int[]nums,int index) {
+		return index == nums.length-1?-1:nums[index+1];
+	}
+}
