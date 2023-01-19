4142a3ff9a6088f01a4bbae4f06318331ad69bb9
Chonticha178 <Chontichakop@gmail.com>
2023-01-19 15:57:48 +0700

    Practice[19-01-2023]

---
+++ OOP_Program/src/AuthorDemo2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class AuthorDemo2 {
+	static Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) {
+		Author author = new Author();
+		for(int i=0;i>=4;i++) {
+			System.out.print("Information Author ");
+		}
+
+	}
+
+}
